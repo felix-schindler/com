acfc79ead9f6e96b8b3fb9f6febd07059bbcd6f6
Felix <felix-schindler@outlook.com>
2022-08-28 20:53:43 +0200

    Update start page and layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,11 +9,93 @@
 <div class="hmf">
 	<header>
 		<nav>
-			<a href="/" class:active={path == "/"}>Home</a>
+			<a href="/" class:active={path == "/"}>Start</a>
+			<a href="/about/team" class:active={path == "/about/team"}>Team</a>
+			<a href="/about/contact" class:active={path == "/about/contact"}>Kontakt</a>
+			<a
+				href="https://github.com/felix-schindler/com"
+				title="Diese Seite auf GitHub"
+				target="_blank"
+				rel="nofollow noreferrer">
+				<i class="bi bi-github" />
+			</a>
 		</nav>
+		<div>
+			<a href="/app/auth">Login</a>
+			<a href="/app/auth/register" role="button">Sign Up</a>
+		</div>
 	</header>
 	<main>
 		<slot />
 	</main>
-	<footer>&copy; 2022 &middot; Felix Schindler</footer>
+	<footer>
+		<span>&copy; 2022 &middot; Felix Schindler</span>
+		<div>
+			<a href="/about/imprint">Impressum</a>
+			<a href="/about/privacy">Datenschutzinformation</a>
+		</div>
+	</footer>
 </div>
+
+<style lang="scss">
+	header {
+		display: flex;
+		align-items: center;
+		justify-content: space-evenly;
+		flex-wrap: wrap;
+
+		div {
+			display: flex;
+			align-items: center;
+			gap: 1em;
+		}
+
+		nav {
+			.bi {
+				vertical-align: baseline;
+			}
+		}
+
+		a[role="button"] {
+			cursor: pointer;
+			color: white;
+			background-color: black;
+			padding: 0.4em 0.6em;
+			border: 1px solid black;
+			border-radius: var(--radius);
+
+			transition: 200ms color, 200ms background-color;
+			text-decoration: none !important;
+
+			&:hover {
+				color: black;
+				background-color: transparent;
+			}
+		}
+	}
+
+	footer {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+
+		div {
+			display: flex;
+			gap: 1em;
+		}
+	}
+
+	@media screen and (prefers-color-scheme: dark) {
+		header {
+			a[role="button"] {
+				color: black;
+				background-color: white;
+				border-color: white;
+
+				&:hover {
+					color: white;
+				}
+			}
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,112 @@
-<h1>Better, Faster, Easier</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let msgs = [
+		"Sie wollen Unternehmensprozesse optimieren?",
+		"Sie wollen ihre eigene Webseite?",
+		"Sie wollen mehr digitalisierung?",
+		"Sie wollen eine Beratung?",
+	];
+
+	let count = 0;
+	let msg = msgs[count];
+
+	setInterval(() => {
+		if (count >= msgs.length) count = 0;
+		else {
+			msg = msgs[count];
+			count++;
+		}
+	}, 3000);
+</script>
+
+<svelte:head>
+	<title>YourCompany</title>
+</svelte:head>
+
+<div class="intro">
+	<h1>{"Faster.\nEasier.\nBetter."}</h1>
+	<h2>{msg}</h2>
+	<h3>Wir helfen!</h3>
+	<!-- <p>
+		Available languages:
+		<button type="button">🇩🇪</button>
+		<button type="button">🇬🇧</button>
+		<button type="button">🇨🇳</button>
+	</p> -->
+</div>
+<div class="grid">
+	<div class="card">
+		<h1>So können wir Ihnen helfen</h1>
+	</div>
+	<div class="card">
+		<h1>Vorteile des Webs</h1>
+		<ol>
+			<li>Immer und von überall erreichbar</li>
+			<li>Kein Update-Albtraum</li>
+			<li>Schnelligkeit</li>
+		</ol>
+	</div>
+	<div class="card">
+		<h1>Warum wir?</h1>
+	</div>
+	<div class="card">
+		<h1>Bisherige Projekte</h1>
+	</div>
+</div>
+
+<style lang="scss">
+	div.intro {
+		width: 95%;
+		margin: 0 auto;
+
+		text-align: center;
+
+		h1 {
+			display: inline-block;
+			margin: 0;
+
+			white-space: pre-wrap;
+			font-size: 25vw;
+
+			color: transparent;
+			background-image: linear-gradient((120deg, var(--accent), blue));
+			background-clip: text;
+			-webkit-background-clip: text;
+			background-size: cover;
+			background-repeat: no-repeat;
+		}
+
+		h2 {
+			margin-bottom: 0.25em;
+		}
+
+		h3 {
+			font-weight: 500;
+			margin-top: 0;
+		}
+
+		// button {
+		// 	cursor: pointer;
+
+		// 	color: inherit;
+		// 	background-color: rgba(var(--gray-rgb), 0.2);
+
+		// 	padding: 0.4em 0.6em;
+		// 	border: 0;
+		// 	border-radius: var(--radius);
+
+		// 	&:hover {
+		// 		background-color: rgba(var(--gray-rgb), 0.4);
+		// 	}
+		// }
+	}
+
+	@media (min-width: 768px) {
+		div.intro {
+			width: 80%;
+
+			h1 {
+				font-size: 10vw;
+			}
+		}
+	}
+</style>
