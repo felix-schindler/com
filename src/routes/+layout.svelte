50f63fca9372c29d30a73a81a05dae71ea76b851
Felix <felix-schindler@outlook.com>
2022-08-28 17:44:34 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import "../style/main.css";
+	import { page } from "$app/stores";
+
+	let path = $page.url.pathname;
+	$: path = $page.url.pathname;
+</script>
+
+<div class="hmf">
+	<header>
+		<nav>
+			<a href="/" class:active={path == "/"}>Home</a>
+		</nav>
+	</header>
+	<main>
+		<slot />
+	</main>
+	<footer>&copy; 2022 &middot; Felix Schindler</footer>
+</div>
