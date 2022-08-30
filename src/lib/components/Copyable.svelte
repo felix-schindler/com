bdf44361d5b52290d7e240a3b331058be80367be
Felix <felix-schindler@outlook.com>
2022-08-30 21:54:35 +0200

    Create Clickable and Copyable components

---
+++ src/lib/components/Copyable.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Clickable from "./Clickable.svelte";
+
+	export let value: string;
+
+	function copy() {
+		navigator.clipboard.writeText(value);
+	}
+</script>
+
+<Clickable onclick={copy}>
+	<slot />
+</Clickable>
+
+<style>
+	:global(.clickable) {
+		cursor: copy !important;
+	}
+</style>
