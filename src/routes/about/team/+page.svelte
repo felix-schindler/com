f02ebe49829bb3a5f988e4f241283df9e4602341
Felix <felix-schindler@outlook.com>
2022-08-30 21:56:02 +0200

    Add person cards of Felix and Florian

--- src/routes/about/team/+page.svelte
+++ src/routes/about/team/+page.svelte
@@ -1,5 +1,30 @@
+<script>
+	import PersonCard from "./PersonCard.svelte";
+</script>
+
 <svelte:head>
 	<title>Unser Team &middot; YourCompany</title>
 </svelte:head>
 
 <h1>Unser Team</h1>
+<div class="grid">
+	<PersonCard
+		name={"Felix Schindler"}
+		links={[
+			{ href: "https://schindlerfelix.de", name: "Persönliche Webseite", icon: "browser-safari" },
+			{ href: "https://github.com/felix-schindler", name: "GitHub", icon: "github" },
+			{ href: "https://gitlab.com/felix-schindler", name: "GitLab", icon: "gitlab" },
+		]} />
+	<PersonCard
+		name={"Florian Schindler"}
+		links={[
+			{ href: "https://schindlerflorian.de", name: "Persönliche Webseite", icon: "browser-chrome" },
+			{ href: "https://github.com/felix-schindler", name: "GitHub", icon: "github" },
+		]} />
+</div>
+
+<style>
+	.grid {
+		grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+	}
+</style>
