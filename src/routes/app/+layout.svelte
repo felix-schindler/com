193c24ba189472fe41905eb462cd0d6dc8ff55a3
Felix <felix-schindler@outlook.com>
2022-08-31 13:35:07 +0200

    Create app/+layout

---
+++ src/routes/app/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { page } from "$app/stores";
+
+	import Footer from "$lib/components/Footer.svelte";
+
+	let path = $page.url.pathname;
+	$: path = $page.url.pathname;
+</script>
+
+<div class="hmf">
+	<header>
+		<nav>
+			<a href="/app" class:active={path == "/app"}>Start</a>
+		</nav>
+	</header>
+	<main>
+		<slot />
+	</main>
+	<Footer />
+</div>
