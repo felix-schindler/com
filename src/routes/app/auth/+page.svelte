ae61e00afa6d19da8adcd70a43e3caa0239d2efb
Felix <felix-schindler@outlook.com>
2022-08-31 13:35:26 +0200

    Del register; Add login page

--- src/routes/app/auth/+page.svelte
+++ src/routes/app/auth/+page.svelte
@@ -1,1 +1,51 @@
-<h1>Login</h1>
+<script lang="ts">
+	import Spinner from "$lib/components/Spinner.svelte";
+
+	let mail: string, pass: string;
+	let success: boolean, action: boolean, loading: boolean;
+
+	/**
+	 * Doesn't return anything but set's `success` and `action`
+	 */
+	function login(): void {
+		loading = true;
+
+		setTimeout(() => {
+			action = true;
+			loading = true;
+			success = false;
+		}, 2500);
+	}
+</script>
+
+<div class="ac">
+	<form on:submit|preventDefault={login} class="card">
+		<h1>Login</h1>
+		<input type="email" bind:value={mail} placeholder="E-Mail" />
+		<input type="password" bind:value={pass} placeholder="Passwort" />
+		<div class="btns">
+			<button type="reset">Clear</button>
+			<button type="submit" disabled={loading || (action && success)}>
+				{#if loading}
+					<Spinner />
+				{/if}
+				Login
+			</button>
+		</div>
+		{#if action && !success}
+			<p>Failed to log in.</p>
+		{/if}
+	</form>
+</div>
+
+<style lang="scss">
+	form.card {
+		display: flex;
+		flex-direction: column;
+
+		.btns {
+			display: grid;
+			grid-template-columns: 1fr 1fr;
+		}
+	}
+</style>
