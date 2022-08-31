1b909039e5a34f224a4d877673b427b215ec758d
Felix <felix-schindler@outlook.com>
2022-08-31 11:25:19 +0200

    Move standard footer to component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,22 @@
+<footer>
+	<span>&copy; 2022 &middot; Felix Schindler</span>
+	<div>
+		<a href="/about/imprint">Impressum</a>
+		<a href="/about/privacy">Datenschutzinformation</a>
+	</div>
+</footer>
+
+<style lang="scss">
+	footer {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		flex-wrap: wrap;
+		gap: 0.5em;
+
+		div {
+			display: flex;
+			gap: 1em;
+		}
+	}
+</style>
