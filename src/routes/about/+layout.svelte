21c7a693a437dbb994d8de62cf365f7f84125478
Felix <felix-schindler@outlook.com>
2022-08-31 11:26:02 +0200

    Move layout and start page to /about

--- src/routes/about/+layout.svelte
+++ src/routes/about/+layout.svelte
@@ -1,16 +1,97 @@
-<div>
-	<slot />
+<script>
+	import "../../style/main.css";
+	import { page } from "$app/stores";
+	import Footer from "$lib/components/Footer.svelte";
+
+	let path = $page.url.pathname;
+	$: path = $page.url.pathname;
+</script>
+
+<div class="hmf">
+	<header>
+		<nav>
+			<a href="/about" class:active={path == "/about"}>Start</a>
+			<a href="/about/team" class:active={path == "/about/team"}>Team</a>
+			<a href="/about/contact" class:active={path == "/about/contact"}>Kontakt</a>
+			<a
+				href="https://github.com/felix-schindler/com"
+				title="Diese Seite auf GitHub"
+				target="_blank"
+				rel="nofollow noreferrer">
+				<i class="bi bi-github" />
+			</a>
+		</nav>
+		<div>
+			<a href="/app/auth">Login</a>
+			<a href="/app/auth/register" role="button">Sign Up</a>
+		</div>
+	</header>
+	<main>
+		<slot />
+	</main>
+	<Footer />
 </div>
 
-<style>
-	div {
+<style lang="scss">
+	header {
+		display: flex;
+		align-items: center;
+		justify-content: space-evenly;
+		flex-wrap: wrap;
+		gap: 0.5em;
+
+		div {
+			display: flex;
+			align-items: center;
+			gap: 1em;
+		}
+
+		nav {
+			.bi {
+				vertical-align: baseline;
+			}
+		}
+
+		a[role="button"] {
+			cursor: pointer;
+			color: white;
+			background-color: black;
+			padding: 0.4em 0.6em;
+			border: 1px solid black;
+			border-radius: var(--radius);
+
+			transition: 200ms color, 200ms background-color;
+			text-decoration: none !important;
+
+			&:hover {
+				color: black;
+				background-color: transparent;
+			}
+		}
+	}
+
+	@media screen and (prefers-color-scheme: dark) {
+		header {
+			a[role="button"] {
+				color: black;
+				background-color: white;
+				border-color: white;
+
+				&:hover {
+					color: white;
+				}
+			}
+		}
+	}
+
+	main {
 		width: 95%;
 		height: 100%;
 		margin: 0 auto;
 	}
 
 	@media (min-width: 768px) {
-		div {
+		main {
 			width: 60%;
 		}
 	}
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,190 @@
+<script>
+	import Copyable from "$lib/components/Copyable.svelte";
+
+	let msgs = [
+		"Sie wollen Unternehmensprozesse optimieren?",
+		"Sie wollen ihre eigene Webseite?",
+		"Sie wollen mehr digitalisierung?",
+		"Sie wollen eine Beratung?",
+	];
+
+	let count = 0;
+	let msg = msgs[count];
+
+	setInterval(() => {
+		if (count >= msgs.length) count = 0;
+		else {
+			msg = msgs[count];
+			count++;
+		}
+	}, 3000);
+</script>
+
+<svelte:head>
+	<title>YourCompany</title>
+</svelte:head>
+
+<div class="intro">
+	<h1>{"Faster.\nEasier.\nBetter."}</h1>
+	<h2>{msg}</h2>
+	<h3>Wir helfen!</h3>
+	<!-- <p>
+		Available languages:
+		<button type="button">🇩🇪</button>
+		<button type="button">🇬🇧</button>
+		<button type="button">🇨🇳</button>
+	</p> -->
+</div>
+<div class="grid">
+	<div class="card">
+		<h1>So können wir Ihnen helfen</h1>
+		<div>
+			<h2>Beratung</h2>
+			<p>Sie wissen noch nicht was sie wollen? Unsere IT-Experten können helfen!</p>
+		</div>
+		<div>
+			<h2>Betreuung</h2>
+			<p>
+				Wir lassen Sie nie im Regeln stehen. Egal was ist und wann es passiert, wir stehen hinter
+				Ihnen und unterstützen Sie.
+			</p>
+		</div>
+		<div>
+			<h2>Entwicklung</h2>
+			<p>Wenn wir etwas entwickeln, dann mit dem Ziel des Kundens im Kopf.</p>
+		</div>
+		<div>
+			<h2>Anbindung</h2>
+			<p>An ihr bestehendes System wie SAP, Sage oder ähnliches</p>
+		</div>
+	</div>
+	<div class="card">
+		<h1>Vorteile des Webs</h1>
+		<div>
+			<h2>Immer und von überall erreichbar</h2>
+			<p>
+				Egal ob unterwegs, beim Kunden, auf dem Klo: Unsere Services sind immer erreichtbar. Selbst
+				offline.
+			</p>
+		</div>
+		<div>
+			<h2>Kein Update-Albtraum</h2>
+			<p>Updates passieren einfach, im Hintergrund. Ohne Sie oder ihre Mitarbeiter zu stören.</p>
+			<p>Keine Update-Buttons, keine veraltete Software mit Sicherheitslücken.</p>
+		</div>
+		<div>
+			<h2>Schnelligkeit</h2>
+			<p>
+				Egal, ob auf einem alten Motorola, einem Rapberry Pi oder dem neuesten Mac, es funktioniert
+				einfach.
+			</p>
+		</div>
+	</div>
+	<div class="card">
+		<h1>Warum wir?</h1>
+		<div>
+			<h2>Angenehm für die Augen</h2>
+			<p>Alle unsere Projekte kommen standardmäßig im dunklen und hellen Modus.</p>
+		</div>
+		<div>
+			<h2>Mobile First</h2>
+			<p>
+				Alle unsere Projekte kommen standardmäßig in allen Formen, damit sie immer alles im Blick
+				haben können.
+			</p>
+		</div>
+		<div>
+			<h2>Best practices</h2>
+			<p>Programmier-Paradigmen die bei uns Anwendung finden: KISS, Never repeat yourself</p>
+		</div>
+		<div>
+			<h2>Best practices</h2>
+			<p>Programmier-Paradigmen die bei uns Anwendung finden: KISS, Never repeat yourself</p>
+		</div>
+		<div>
+			<h2>Das Auge liegt im Detail</h2>
+			<em>(pictures of notch, cookie banner, install prompt android, light house)</em>
+		</div>
+		<div>
+			<h2>Sie haben nichts zu befürchten</h2>
+			<p>Wir kümmern uns um Gesetzeskonformität</p>
+		</div>
+	</div>
+	<div class="card">
+		<h1>Bisherige Projekte</h1>
+	</div>
+	<div class="card">
+		<h1>Weil wir's können.</h1>
+		<div class="half">
+			<div>
+				<h2>Serverless (Cloud)</h2>
+			</div>
+			<div>
+				<h2>Server</h2>
+			</div>
+		</div>
+		<div class="half">
+			<div>
+				<h2>Frontend</h2>
+				<p>
+					Use you're cursor, <Copyable value={"you can copy this."}>make a copy.</Copyable>
+				</p>
+			</div>
+			<div>
+				<h2>Backend</h2>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	div.intro {
+		width: 95%;
+		margin: 0 auto;
+
+		text-align: center;
+
+		h1 {
+			display: inline-block;
+			margin: 0;
+
+			white-space: pre-wrap;
+			font-size: 15vh;
+
+			color: transparent;
+			background-image: linear-gradient((120deg, var(--accent), blue));
+			background-clip: text;
+			-webkit-background-clip: text;
+			background-size: cover;
+			background-repeat: no-repeat;
+		}
+
+		h2 {
+			margin-bottom: 0.25em;
+		}
+
+		h3 {
+			font-weight: 500;
+			margin-top: 0;
+		}
+	}
+
+	.grid {
+		grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+	}
+
+	@media (min-width: 1366px) {
+		div.intro {
+			width: 80%;
+
+			h1 {
+				font-size: 22.5vh;
+			}
+		}
+
+		.half {
+			display: flex;
+			gap: 1em;
+		}
+	}
+</style>
