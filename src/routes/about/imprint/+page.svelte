0204895440f5005d03a6fda2aaa98de8bc520d33
Felix <felix-schindler@outlook.com>
2022-09-07 12:18:23 +0200

    [imprint & privacy]: Add `word-break` and `hyphens`

--- src/routes/about/imprint/+page.svelte
+++ src/routes/about/imprint/+page.svelte
@@ -2,28 +2,36 @@
 	<title>Impressum &middot; YourCompany</title>
 </svelte:head>
 
-<h1>Impressum</h1>
-<address>
-	<p><b><i class="bi bi-mailbox" /> Felix Schindler</b></p>
-	<p>Theodor-Veiel-Straße 63</p>
-	<p>70374 Stuttgart</p>
-</address>
-<section>
-	<p>
-		Telefon:
-		<a href="https://t.me/felix_schindler" rel="nofollow noreferrer">
-			<i class="bi bi-telegram" /> felix_schindler
-		</a>
-	</p>
-	<p>
-		E-Mail:
-		<a href="mailto:webmaster@schindlerfelix.de">
-			<i class="bi bi-envelope" /> webmaster@schindlerfelix.de
-		</a>
-	</p>
-</section>
+<div>
+	<h1>Impressum</h1>
+	<address>
+		<p><b><i class="bi bi-mailbox" /> Felix Schindler</b></p>
+		<p>Theodor-Veiel-Straße 63</p>
+		<p>70374 Stuttgart</p>
+	</address>
+	<section>
+		<p>
+			Telefon:
+			<a href="https://t.me/felix_schindler" rel="nofollow noreferrer">
+				<i class="bi bi-telegram" /> felix_schindler
+			</a>
+		</p>
+		<p>
+			E-Mail:
+			<a href="mailto:webmaster@schindlerfelix.de">
+				<i class="bi bi-envelope" /> webmaster@schindlerfelix.de
+			</a>
+		</p>
+	</section>
+</div>
 
 <style lang="scss">
+	div {
+		word-break: break-word;
+		-webkit-hyphens: auto;
+		hyphens: auto;
+	}
+
 	address,
 	section {
 		&:not(:last-child) {
--- src/routes/about/privacy/+page.svelte
+++ src/routes/about/privacy/+page.svelte
@@ -2,14 +2,22 @@
 	<title>Datenschutzinformation &middot; YourCompany</title>
 </svelte:head>
 
-<h1>Datenschutzinformation</h1>
-<p>
-	Da diese Webseite bisher noch nicht für die Öffentlichkeit gedacht ist, gibt es bisher keine
-	Datenschutzinformationen. Fall's Sie Fragen haben, melden Sie sich jederzeit gerne per E-Mail
-	<a href="mailto:webmaster@schindlerfelix.de">(webmaster@schindlerfelix.de)</a>.
-</p>
+<div>
+	<h1>Datenschutzinformation</h1>
+	<p>
+		Da diese Webseite bisher noch nicht für die Öffentlichkeit gedacht ist, gibt es bisher keine
+		Datenschutzinformationen. Falls Sie Fragen haben, melden Sie sich jederzeit gerne per E-Mail
+		<a href="mailto:webmaster@schindlerfelix.de">(webmaster@schindlerfelix.de)</a>.
+	</p>
+</div>
 
 <style>
+	div {
+		word-break: break-word;
+		-webkit-hyphens: auto;
+		hyphens: auto;
+	}
+
 	a:hover {
 		color: var(--accent);
 	}
