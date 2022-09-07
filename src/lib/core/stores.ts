761ff9b90b80984af12871d31f3d7c7caf243fc9
Felix <felix-schindler@outlook.com>
2022-09-07 10:40:27 +0200

    Add nerd (ls) and pocketbase stores

--- src/lib/core/stores.ts
+++ src/lib/core/stores.ts
@@ -1,3 +1,9 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
+import { writable as ls_writable } from "svelte-local-storage-store";
+import PocketBase from "pocketbase";
 
 export const authStore = writable({ model: null });
+
+export const nerd = ls_writable("nerd", false);
+
+export const pb = new PocketBase("https://pb.schindlerfelix.de");
