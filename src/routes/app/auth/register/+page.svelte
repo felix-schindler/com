5105353645c24258ded06b7a2cf76ecebd05d6d4
Felix <felix-schindler@outlook.com>
2022-09-14 21:51:38 +0200

    Make use of the new components

--- src/routes/app/auth/register/+page.svelte
+++ src/routes/app/auth/register/+page.svelte
@@ -1,38 +1,52 @@
 <script lang="ts">
 	import type { Question, SimpleQuestion, GivenAnswer } from "$lib/core/types";
 
+	import Quiz from "./Quiz.svelte";
+	import Evaluation from "./Evaluation.svelte";
+	import Spinner from "$lib/components/Spinner.svelte";
+	import { page } from "$app/stores";
+
 	let allQuestions: Question[];
 	let currentQuestion: SimpleQuestion;
 
 	let lastQuestionAnswered = false;
 
-	let mail: string; // mail address of registering user
-	let answers: GivenAnswer[];
+	let answers: GivenAnswer[] = [];
 
 	async function loadQuestions(): Promise<void> {
-		const res = await fetch("/app/auth/register/quiz");
+		const res = await fetch($page.url.href + "/quiz");
 		allQuestions = await res.json();
 		currentQuestion = allQuestions[0];
 	}
 
-	async function answer() {}
-
-	async function saveAnswers() {
-		const { pb } = await import("$lib/core/stores");
-
-		if (lastQuestionAnswered) {
-			// save to db
-			pb.records.create("register_answers", {
-				mail,
-				answers,
-			});
-		}
-	}
+	loadQuestions();
 </script>
 
 <div class="ac">
 	<div class="card">
-		{#if lastQuestionAnswered}{/if}
 		<h1>Willkommen!</h1>
+		{#if lastQuestionAnswered}
+			<Evaluation bind:answers />
+		{:else if currentQuestion}
+			<Quiz bind:currentQuestion bind:answers />
+		{:else}
+			<Spinner /> Lädt...
+		{/if}
 	</div>
 </div>
+
+<style lang="scss">
+	.ac {
+		padding: 0 0.5em;
+
+		& > .card {
+			width: 100%;
+			text-align: center;
+
+			@media (min-width: 1024px) {
+				min-width: 525px;
+				max-width: 50%;
+			}
+		}
+	}
+</style>
