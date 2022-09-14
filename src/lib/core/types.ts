b357572a083a3c29ab2f2940108e270187bbde84
Felix <felix-schindler@outlook.com>
2022-09-14 21:51:05 +0200

    Add some more types for the register quiz

--- src/lib/core/types.ts
+++ src/lib/core/types.ts
@@ -1,12 +1,27 @@
-export type Quiz = {
-	index: number;
-	max: number;
+export type Question = {
 	question: string;
 	answers: Answer[];
 };
 
 export type Answer = {
-	index: number;
+	answer: string;
+	hint?: string;
+	subQuestions?: Question[];
+};
+
+
+export type SimpleQuestion = {
+	question: string;
+	answers: SimpleAnswer[];
+};
+
+export type SimpleAnswer = {
 	answer: string;
 	hint?: string;
 };
+
+export type GivenAnswer = {
+	question: string;
+	answer: string;
+};
+
