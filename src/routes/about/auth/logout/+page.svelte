c62d52f37ffc12eb0056b5e015b95d208dfe9e6c
Felix <felix-schindler@outlook.com>
2022-10-03 01:20:14 +0200

    Create `about/auth/logout` page

---
+++ src/routes/about/auth/logout/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { browser } from "$app/environment";
+	import { goto } from "$app/navigation";
+	import { authStore, pb } from "$lib/core/stores";
+	import { redirect } from "@sveltejs/kit";
+
+	try {
+		pb.authStore.clear();
+		authStore.set({ model: null });
+	} catch {}
+
+	if ($authStore.model === null) {
+		const aboutPath = "/about";
+		if (browser) goto(aboutPath);
+		else redirect(307, aboutPath);
+	}
+</script>
