8adf891eaeb6ca4a3f75a5cbeec749c1b5898cd2
Felix <felix-schindler@outlook.com>
2022-10-04 23:18:55 +0200

    Add global `.clickable` style
    TODO: `Clickable` component is probably no longer needed

--- src/lib/components/Clickable.svelte
+++ src/lib/components/Clickable.svelte
@@ -5,24 +5,3 @@
 <span class="clickable" on:click={onclick}>
 	<slot />
 </span>
-
-<style lang="scss">
-	.clickable {
-		cursor: pointer;
-
-		padding: 3.2px 6.4px;
-		border-radius: var(--radius);
-
-		&:hover:not(:disabled) {
-			background-color: rgba(var(--gray-rgb), 0.4);
-		}
-
-		&:active:not(:disabled) {
-			background-color: rgba(var(--gray-rgb), 0.6);
-		}
-
-		&:disabled {
-			background-color: rgba(var(--gray-rgb), 0.3);
-		}
-	}
-</style>
