88606af5262044d4d564508fc9c6bbb935fbbad3
Felix <felix-schindler@outlook.com>
2022-10-08 23:40:02 +0200

    Replace `'` with `"`

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,19 @@
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
-import { mdsvex } from 'mdsvex';
+import { mdsvex } from "mdsvex";
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
 		preprocess(),
 		mdsvex({
-			extensions: ['.md']
+			extensions: [".md"]
 		})
 	],
 
-	extensions: ['.svelte', '.md'],
+	extensions: [".svelte", ".md"],
 
 	kit: {
 		version: {
