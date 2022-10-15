9a75b164b13fb0fab81a35ee4649bda4072995c0
Felix <felix-schindler@outlook.com>
2022-10-15 18:17:26 +0200

    `/about`: :warning: Remove many things
    This deletes many things to be cleaner and clearer.
    
    closes #15

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,10 +1,8 @@
 <script>
-	import Clickable from "$lib/components/Clickable.svelte";
-
 	const msgs = [
 		"Sie wollen Unternehmensprozesse optimieren?",
 		"Sie wollen ihre eigene Webseite?",
-		"Sie wollen mehr digitalisierung?",
+		"Sie wollen mehr Digitalisierung?",
 		"Sie wollen eine Beratung?",
 	];
 
@@ -61,7 +59,7 @@
 			<h2>Anbindung</h2>
 			<p>
 				Sie haben bereits ein bestehendes System? Wir können helfen in dem wir es an eine neue
-				Oberfläche anbinden oder generalüberholen.
+				Oberfläche anbinden.
 			</p>
 		</div>
 	</div>
@@ -70,8 +68,8 @@
 		<div>
 			<h2>Immer und von überall erreichbar</h2>
 			<p>
-				Egal ob unterwegs, beim Kunden oder auf dem Klo, unsere Services sind immer erreichtbar.
-				Selbst offline.
+				Egal ob unterwegs, beim Kunden oder auf dem Klo. Unsere Services sind immer erreichtbar,
+				selbst offline!
 			</p>
 		</div>
 		<div>
@@ -93,170 +91,39 @@
 	<div class="card">
 		<h1>Warum wir?</h1>
 		<div>
-			<details>
-				<summary><h2>Auge für's Detail</h2></summary>
-				<div class="card">
-					<h3>Angenehm für die Augen</h3>
-					<p>
-						Alle unsere Projekte kommen standardmäßig im hellen und dunklen Modus. Dies ist angenehm
-						für die Augen, kann aber zum Beispiel auch Akku sparen.
-					</p>
-				</div>
-				<div class="card">
-					<h3>Erfahrung</h3>
-					<p>
-						Durch jahrelange Erfahrung wissen wir, was wir tun.
-						<em>(pictures of notch, cookie banner, install prompt android, light house)</em>
-						<img
-							src="/img/why-us/lighthouse.gif"
-							alt="Perfect Google Lighthouse (Website Speedtest) Result"
-							loading="lazy" />
-					</p>
-				</div>
-				<div class="card">
-					<h3>Durckbar - Printable</h3>
-					<p>
-						Wir achten generell drauf, dass unsere Webseiten ausgedruckt werden können, ohne viel
-						Tinte zu verschwenden.
-					</p>
-				</div>
-				<div class="card">
-					<h3>i18n</h3>
-					<div>
-						Das Datum ist immer im richtigen Format. Die Entfernung und Temparatur ist in einer
-						Einheit, die man sich vorstellen kann. Große Zahlen sind richtig getrennt und beim
-						ersten Blick gelesen.
-					</div>
-					<div class="row">
-						<div class="card" lang="de">
-							<h4>Deutsch</h4>
-							<p>{Number(12345.67).toLocaleString("de-DE")} €</p>
-							<p>{Number(37).toLocaleString("de-DE")} °C</p>
-							<p>{Number(10).toLocaleString("de-DE")} KM</p>
-							<p>{new Date().toLocaleString("de-DE", { hourCycle: "h23" })}</p>
-							<p>
-								{new Date().toLocaleString("de-DE", {
-									hourCycle: "h23",
-									dateStyle: "medium",
-									timeStyle: "short",
-								})}
-							</p>
-							<ol>
-								<li>Erstens</li>
-								<li>Zweitens</li>
-							</ol>
-						</div>
-						<div class="card" lang="en">
-							<h4>English</h4>
-							<p>{Number(12345.67).toLocaleString("en-US")} $</p>
-							<p>{Number(98.6).toLocaleString("en-US")} °F</p>
-							<p>{Number(6.21).toLocaleString("en-US")} miles</p>
-							<p>{new Date().toLocaleString("en-US", { hourCycle: "h11" })}</p>
-							<p>
-								{new Date().toLocaleString("en-US", {
-									hourCycle: "h11",
-									dateStyle: "medium",
-									timeStyle: "short",
-								})}
-							</p>
-							<ol>
-								<li>First</li>
-								<li>Second</li>
-							</ol>
-						</div>
-						<div class="card" lang="zh-Hans">
-							<h4>中文</h4>
-							<p>{Number(12345.67).toLocaleString("zh-Hans")} ¥</p>
-							<p>{Number(37).toLocaleString("zh-Hans")} 摄氏度</p>
-							<p>{Number(10).toLocaleString("zh-Hans")} 公里</p>
-							<p>
-								{new Date().toLocaleString("zh-Hans", {
-									calendar: "chinese",
-									hourCycle: "h11",
-								})}
-							</p>
-							<p>
-								{new Date().toLocaleString("zh-Hans", {
-									calendar: "chinese",
-									hourCycle: "h11",
-									dateStyle: "medium",
-									timeStyle: "short",
-								})}
-							</p>
-							<ol>
-								<li>第一</li>
-								<li>第二</li>
-							</ol>
-						</div>
-					</div>
-					<p>
-						Sie haben Mitarbeiter aus anderen Ländern? Kein Problem! Unsere Seiten können in allen
-						möglichen Sprachen kommen. Außerdem stimmen bei uns Dinge wie Datumsformat immer.
-					</p>
-				</div>
-			</details>
-			<details>
-				<summary><h2>Weil wir's können</h2></summary>
-				<div class="card">
-					<h3>Frontend und Backend</h3>
-					<p>Unsere Frontends sind Benutzer-Zentriert, einfach und schnell.</p>
-					<p>
-						Unser Backends sind skalierbar und halten sich an allgemeine Web-Standards, einfach
-						anzupassen und zu erweitern.
-					</p>
-				</div>
-				<div class="card">
-					<h3>Server oder Serverless</h3>
-					<p>
-						Sie wollen ihre Daten nicht in der Cloud, sondern lieber hier auf dem Boden? Wir kümmern
-						uns um Server und, dass alles läuft.
-					</p>
-					<p>
-						Sie erwarten eine Menge Benutzer und wollen schon jetzt in die Zukunft investieren? Wir
-						sind vertraut mit Cloud-Lösungen und können diese Problemlos ansteuern.
-					</p>
-				</div>
-			</details>
-			<details>
-				<summary><h2>Keine unnötige Arbeit</h2></summary>
-				<div class="card">
-					<h3>Wir erfinden das Rad nicht neu</h3>
-					<p>
-						Wir nutzen das, was uns Betriebssystem und Browser hergeben. Dadurch steuern wir native
-						APIs an, die dem User bereits vertraut vorkommen.
-					</p>
-					<p>
-						Beispiele: Dunkler Modus, <Clickable
-							onclick={() =>
-								navigator.share({
-									title: 'Beispiel Dialog "Teilen"',
-									text: "Hier steht ein Text",
-								})}>Share Dialog</Clickable>
-					</p>
-				</div>
-				<div class="card">
-					<h3>Barrierefreiheit</h3>
-					<p>
-						Unsere Webseiten lassen sich auch benutzen, wenn man nur eine Tastatur zur Verfügung
-						hat. Oder einen Screenreader.
-					</p>
-				</div>
-			</details>
-		</div>
-		<div>
-			<h2>Mobile First</h2>
-			<p>
-				Alle unsere Projekte kommen standardmäßig in allen Formen, damit sie immer alles im Blick
-				haben können.
-			</p>
-		</div>
-		<div>
-			<h2>Best practices</h2>
-			<p>Programmier-Paradigmen die bei uns Anwendung finden: KISS, Never repeat yourself</p>
-		</div>
-		<div>
-			<h2>Gesetzeskonformität</h2>
-			<p>Wir haben aktuelle Gesetze im Blick, und setzen sie direkt um.</p>
+			<div>
+				<h2>Erfahrung</h2>
+				<p>
+					Wir nutzen das, was uns Betriebssystem und Browser hergeben. Wir steuern native APIs an,
+					die dem User bereits vertraut vorkommen und sparen zusätzlich Zeit.
+				</p>
+				<p>
+					Durch jahrelange Erfahrung wissen wir, was wir tun.
+					<em>(pictures of notch, cookie banner, install prompt android, light house)</em>
+					<img
+						src="/img/why-us/lighthouse.gif"
+						alt="Perfect Google Lighthouse (Website Speedtest) Result"
+						loading="lazy" />
+				</p>
+			</div>
+			<div>
+				<h2>Weil wir's können.</h2>
+				<h3>Frontend und Backend</h3>
+				<p>Unsere Frontends sind Benutzer-Zentriert, einfach und schnell.</p>
+				<p>
+					Unser Backends sind skalierbar und halten sich an allgemeine Web-Standards, einfach
+					anzupassen und zu erweitern.
+				</p>
+				<h3>Server oder Serverless</h3>
+				<p>
+					Sie wollen ihre Daten nicht in der Cloud, sondern lieber hier auf dem Boden? Wir kümmern
+					uns um Server und, dass alles läuft.
+				</p>
+				<p>
+					Sie erwarten eine Menge Benutzer und wollen schon jetzt in die Zukunft investieren? Wir
+					sind vertraut mit Cloud-Lösungen und können diese Problemlos ansteuern.
+				</p>
+			</div>
 		</div>
 	</div>
 </div>
@@ -308,21 +175,6 @@
 			img {
 				max-width: 100%;
 				height: auto;
-			}
-
-			details {
-				summary {
-					cursor: pointer;
-
-					h2 {
-						display: inline-block;
-						margin-block: 0.2em;
-					}
-				}
-
-				div.card:not(:last-child) {
-					margin-bottom: 0.5em;
-				}
 			}
 		}
 	}
