729936e7db066fcd36d2a5f563501d7b11eb196b
Felix <felix-schindler@outlook.com>
2022-10-18 15:48:16 +0200

    `/app/settings`: Create ChangeMail component

---
+++ src/routes/app/settings/ChangeMail.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { authStore } from "$lib/core/stores";
+
+	let mail: string | undefined = $authStore?.email;
+	let dialog: HTMLDialogElement;
+</script>
+
+<button type="button" on:click={() => dialog.showModal()}>
+	<i class="bi bi-envelope" /> Change e-Mail
+</button>
+<dialog bind:this={dialog}>
+	<button class="close" type="button" on:click={() => dialog.close()}>❌</button>
+
+	<form on:submit|preventDefault={() => {}}>
+		<input type="email" bind:value={mail} placeholder="New e-Mail" disabled />
+		<button type="submit" disabled>Change e-Mail</button>
+	</form>
+</dialog>
+
+<style lang="scss">
+	dialog {
+		width: 100%;
+		border-radius: var(--radius);
+
+		@media (min-width: 1024px) {
+			min-width: 525px;
+			max-width: 25%;
+		}
+
+		button.close {
+			position: absolute;
+			top: 0.5em;
+			right: 0.5em;
+
+			border: 0;
+			padding: 0.4em;
+			background-color: rgba(var(--gray-rgb), 0.2);
+
+			&:hover {
+				background-color: rgba(var(--gray-rgb), 0.3);
+			}
+
+			&:active {
+				background-color: rgba(var(--gray-rgb), 0.4);
+			}
+		}
+	}
+</style>
